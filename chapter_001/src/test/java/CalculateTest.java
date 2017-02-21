f5a971f7fe4b2e0cc3de41fbf512b78e16aae2e2
Nikolay Kisil <lakismile.me@gmail.com>
2017-02-20 13:50:24 -1200

    test test test

---
+++ chapter_001/src/test/java/CalculateTest.java
@@ -0,0 +1,33 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+	*Class Calculate решение задачи части 001 урок 1.
+	*@author kisil
+	*since 15.02.2017
+*/
+public class CalculateTest {
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(
+                out.toString(),
+                is(
+                        String.format(
+                                "Hello World%s",
+                                System.getProperty("line.separator")
+                        )
+                )
+        );
+    }
+}
